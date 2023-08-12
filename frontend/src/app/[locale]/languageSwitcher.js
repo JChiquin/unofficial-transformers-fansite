368e6cf17669a66b115ec6719b32915a1dbde411
Jorge Chiquin <Jorge.Chiquin@umusic.com>
2023-08-12 00:00:07 -0400

    Finishing laguange switcher

--- frontend/src/app/[locale]/languageSwitcher.js
+++ frontend/src/app/[locale]/languageSwitcher.js
@@ -2,36 +2,40 @@
 
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
 
-const languages = ['es', 'en']
+const languages = [
+  {
+    locale: 'es',
+    icon: "https://www.pinclipart.com/picdir/big/102-1028023_spain-clip-art.png",
+  }, {
+    locale: 'en',
+    icon: "https://cdn4.iconfinder.com/data/icons/iconsimple-flagtypes/512/united_states-256.png",
+  }]
 
 export default function LocaleSwitcher() {
   const pathName = usePathname()
   const redirectedPathName = (locale) => {
     if (!pathName) return '/'
     let segments = pathName.split('/')
-    console.log(locale)
-    console.log(segments)
-    if (languages.some((lang) => lang == segments[1])) {
-        segments[1] = locale
-    } else {
-        segments[2] = locale
-    }
+    segments[1] = locale
     return segments.join('/')
+    //if (languages.some((lang) => lang == segments[1])) {
+    //}
+    //return `/${locale}${pathName}`
+
   }
 
   return (
-    <div>
-      <p>Locale switcher:</p>
-      <ul>
-        {languages.map((locale) => {
-          return (
-            <li key={locale}>
-              <Link href={redirectedPathName(locale)}>{locale}</Link>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
+    <Box>
+      {languages.map((language) => {
+        return (
+          <IconButton LinkComponent={Link} href={redirectedPathName(language.locale)} color="inherit">
+            <img width={30} src={language.icon}/>
+          </IconButton>
+        )
+      })}
+    </Box>
   )
 }
